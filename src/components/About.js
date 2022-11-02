74b6f1f853340a8d08f20bf53dd29aee30612c52
Marco Amador <marcoamador214@gmail.com>
2022-11-02 09:46:35 -0700

    Core Deliverables

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import blogData from '../data/blog';
+
+function About( {image = "https://via.placeholder.com/215"}) {
+    return (
+       <aside>
+            <img src={blogData.image} alt="blog logo"/>
+            <p>{blogData.about}</p>
+       </aside> 
+    )
+}
+
+export default About;
